cb4ed7a18432f55d2712083d0514e05fca83aa10
Nitin Mukesh Tiwari <nmtiwari@iastate.edu>
2016-09-10 23:16:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-'use strict';	
+'use strict';
 let json = api.boa.run('number_of_Static-not-Final.boa');
 $('#loading').hide();
 $('#content').show();
 let count = 0;
 let labels = [];
 let dataset = [];
-for(let index in json.out["[]"]) {
+for(let index in json.out) {
 	count++;
-	let label = 
-		$('#table-output-body').append(`<tr><td> ${count} </td> <td> ${index} </td> <td> ${json.out["[]"][index]} </td> </tr>`)
+	let label =
+		$('#table-output-body').append(`<tr><td> ${count} </td> <td> ${index} </td> <td> ${json.out[index]} </td> </tr>`)
 		labels.push(index);
-	dataset.push(json.out["[]"][index]);
+	dataset.push(json.out[index]);
 }
 let chartData = {
 		labels: labels,
@@ -41,7 +41,3 @@ content.appendChild(canvas);
 
 let ctx = canvas.getContext('2d');
 new Chart(ctx).Bar(chartData);
-
-
-
-
